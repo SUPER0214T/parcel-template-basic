de833f382ac666ad1a7263539464aacac58d4393
SUPEREPT <twinjyjh2@gmail.com>
2021-07-22 21:45:03 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,8 @@
+// 아래의 코드는 import autoprefixer from 'autoprefixer'과 같다고 보면 된다.
+// const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
